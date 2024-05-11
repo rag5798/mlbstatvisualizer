e948845b9bded729acf6e00f4ae75e3d0a196061
rag5798 <bgoettman@yahoo.com>
2024-05-11 17:18:35 -0600

    added comments to explain code

--- sprint1_final.R
+++ sprint1_final.R
@@ -3,28 +3,39 @@ library(gapminder)
 library(dplyr)
 library(rvest)
 
+#Grabs the HTML from the mlb site
 batting_link <- "https://www.mlb.com/stats/"
 batting_page <- batting_link %>% read_html()
 
+#from the html page this grabs eveything with the class of .full-3fV3c9pF in the html (This is put into a list)
 names <- batting_page %>% html_nodes(".full-3fV3c9pF") %>% html_text()
 #names
+
+#This combines the first elemnent in the list with the second and steps down the list by 2 to combine each name
 full_name <- paste(names[seq(1, length(names), by = 2)], names[seq(2, length(names), by = 2)])
 #full_name
 
+#Grabs the postion text with the class of .position-28TbwVOg in the html (This is put into a list)
 position <- batting_page %>% html_nodes(".position-28TbwVOg") %>% html_text()
 #position
 
+#grabs the team from the site with a class of .align-left-3L2SU-Mk (This is put into a list)
 team <- batting_page %>% html_nodes(".align-left-3L2SU-Mk") %>% html_text() %>% .[-1]
 
+#This grabs all the values for the stats using the class bellow
 stats <- batting_page %>% html_nodes(".bui-table td") %>% html_text()
+#This Iterates through the list to filter out all the team names it grabs
 for (x in seq_along(stats)) {
   if (stats[x] %in% team) {
     stats[x] <- NA
   }
 }
+#Na.omit removes all indexs that are NULL and shifts the data accourdingly
 stats <- na.omit(stats)
 #stats
 
+#This data frame names each column and sets it equal to an index and then steps by 16 to get the remaining values in that colmun
+#There are 16 values in the table so I have to step by 16
 batting_stats <- data.frame(
   G = stats[seq(1, length(stats), by = 16)],
   AB = stats[seq(2, length(stats), by = 16)],
@@ -46,12 +57,20 @@ batting_stats <- data.frame(
 )
 #View(batting_stats)
 
+#This creates the player data frame which is only the full name that I previously made
+#the position and the team values. This iterates through each list and creates the rows
+#For example, index 1 for all 3 lists make up the first row of the frame
 player <- data.frame(full_name, position, team, stringsAsFactors = FALSE)
 #View(player)
 
+#cbind is used to bind columns of tables together
+#This puts the player names, team, and position with the respective stats
+#One thing to not is that the rows have to match in a cbind()
 full_table <- cbind(player, batting_stats)
 #View(full_table)
 
+#This section is similar to the batting section where it gets the data
+#filters out unwanted data and combines them in a data frame
 pitching_link <- "https://www.mlb.com/stats/pitching?sortState=asc"
 pitching_page <- pitching_link %>% read_html()
 
@@ -88,6 +107,8 @@ pitching_players <- data.frame(
 )
 #View(pitching_players)
 
+#This is how to create functions in R
+#This function takes in the links, gets the data from the html, and makes a data frame, similar to above
 batting_links <- function(links){
   new_page <- paste("https://www.mlb.com/stats/?page=", links, sep = "") %>% read_html()
   new_names <- new_page %>% html_nodes(".full-3fV3c9pF") %>% html_text()
@@ -95,14 +116,12 @@ batting_links <- function(links){
   new_position <- new_page %>% html_nodes(".position-28TbwVOg") %>% html_text()
   new_team <- new_page %>% html_nodes(".align-left-3L2SU-Mk") %>% html_text() %>% .[-1]
   new_stats <- new_page %>% html_nodes(".bui-table td") %>% html_text()
-
   for (x in seq_along(new_stats)) {
     if (new_stats[x] %in% new_team) {
       new_stats[x] <- NA
     }
   }
   new_stats <- na.omit(new_stats)
-  
   new_batting_stats <- data.frame(
     G = new_stats[seq(1, length(new_stats), by = 16)],
     AB = new_stats[seq(2, length(new_stats), by = 16)],
@@ -122,12 +141,13 @@ batting_links <- function(links){
     OPS = new_stats[seq(16, length(new_stats), by = 16)],
     stringsAsFactors = FALSE
   )
-  
   new_player <- data.frame(new_full_name, new_position, new_team, stringsAsFactors = FALSE)
   new_full_table <- cbind(new_player, new_batting_stats)
+  #This returns a new table that can be set to a new variable also shown bellow
   return(new_full_table)
 }
 
+#This is similar to the function above but grabs all the pitching information
 pitching_stats <- function(links){
   new_pitching_link <- paste("https://www.mlb.com/stats/pitching?page=", links, "&sortState=asc", sep = "")
   new_pitching_page <- new_pitching_link %>% read_html()
@@ -165,17 +185,13 @@ pitching_stats <- function(links){
   return(new_pitching_players)
 }
 
-
+#This grabs how many pages are in the batting section of the stats and puts it into the function above
 links <- batting_page %>% html_nodes(".bui-button.tab-27nhZTIl span") %>% html_text()
 links <- links[-1:-3]
 #links
-
-if (1 %in% links) {
-  links <- links[links != 1]
-}
-
 #View(full_table)
 
+#This 
 for (x in links) {
   new_full_table <- batting_links(x)
   names(new_full_table) <- names(full_table)
@@ -183,6 +199,7 @@ for (x in links) {
   full_table <- rbind(full_table, new_full_table)
 }
 
+#This grabs how many pages are in the batting section of the stats and puts it into the function above
 pitching_pagenums <- pitching_page %>% html_nodes(".bui-button.tab-27nhZTIl .button-3wq5VxsJ.sm-1_yhMOW5 span") %>% html_text()
 pitching_pagenums <- pitching_pagenums[-1:-3]
 #pitching_pagenums
@@ -194,24 +211,32 @@ for (x in pitching_pagenums){
 #View(pitching_players)
 #View(full_table)
 
+#This visualizes the team with the best batters using the OBP column of the batting data frame
 #Which team has the best batters based on OBP?
 full_table$OBP <- as.numeric(full_table$OBP)
 #summary(full_table$OBP)
+
+#This plot takes the OBP column and makes a vertical box and whisker plot graph
+#Meaning that the mean and range are displayed for each team
 ggplot(full_table, aes(x = team, y = OBP)) +
   geom_boxplot(fill = "skyblue", color = "blue") +
+  #This creates a line across the entire graph denoting the mean of the entire OBP column
   geom_hline(yintercept = mean(full_table$OBP), color = "red", linetype = "dashed") +
-  labs(title = "Distribution of OBP by Team",
+  labs(title = "Best Batters based on OBP by Team",
        x = "Team",
        y = "OBP") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+
+##This visualizes the team with the best pitchers using the ERA column of the pitching data frame
 #Which team has the best pitchers based on ERA?
 pitching_players$ERA <- as.numeric(pitching_players$ERA)
 #summary(pitching_players$ERA)
+#This does the exact same thing as the plot above but with the ERA column from the pitching data frame
 ggplot(pitching_players, aes(x = Team, y = ERA)) +
   geom_boxplot(fill = "skyblue", color = "blue") +
   geom_hline(yintercept = mean(pitching_players$ERA, na.rm = TRUE), color = "red", linetype = "dashed") +
-  labs(title = "Distribution of ERA by Team",
+  labs(title = "Best Pitchers based on ERA by Team",
        x = "Team",
        y = "ERA") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
